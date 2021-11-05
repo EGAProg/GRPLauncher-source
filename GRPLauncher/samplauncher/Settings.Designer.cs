3cd6d139ed365f9571f69056a47d5e69e878fcfb
Egor <93545142+EGAProg@users.noreply.github.com>
2021-11-05 14:48:00 +0300

    Add

---
+++ GRPLauncher/samplauncher/Settings.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace SAMPLauncher
+{
+    partial class Settings
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Settings));
+            this.tbPath = new System.Windows.Forms.TextBox();
+            this.lPath = new System.Windows.Forms.Label();
+            this.bPath = new System.Windows.Forms.Button();
+            this.bDone = new System.Windows.Forms.Button();
+            this.cbExitOnStart = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // tbPath
+            // 
+            this.tbPath.Location = new System.Drawing.Point(16, 39);
+            this.tbPath.Margin = new System.Windows.Forms.Padding(4);
+            this.tbPath.Name = "tbPath";
+            this.tbPath.Size = new System.Drawing.Size(256, 22);
+            this.tbPath.TabIndex = 0;
+            this.tbPath.TextChanged += new System.EventHandler(this.tbPath_TextChanged);
+            // 
+            // lPath
+            // 
+            this.lPath.AutoSize = true;
+            this.lPath.Location = new System.Drawing.Point(16, 16);
+            this.lPath.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lPath.Name = "lPath";
+            this.lPath.Size = new System.Drawing.Size(47, 17);
+            this.lPath.TabIndex = 1;
+            this.lPath.Text = "Путь: ";
+            // 
+            // bPath
+            // 
+            this.bPath.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.bPath.FlatAppearance.BorderSize = 0;
+            this.bPath.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bPath.ForeColor = System.Drawing.Color.Black;
+            this.bPath.Location = new System.Drawing.Point(281, 39);
+            this.bPath.Margin = new System.Windows.Forms.Padding(4);
+            this.bPath.Name = "bPath";
+            this.bPath.Size = new System.Drawing.Size(53, 25);
+            this.bPath.TabIndex = 2;
+            this.bPath.Text = "...";
+            this.bPath.UseVisualStyleBackColor = false;
+            this.bPath.Click += new System.EventHandler(this.bPath_Click);
+            // 
+            // bDone
+            // 
+            this.bDone.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.bDone.FlatAppearance.BorderSize = 0;
+            this.bDone.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bDone.ForeColor = System.Drawing.Color.Black;
+            this.bDone.Location = new System.Drawing.Point(125, 71);
+            this.bDone.Margin = new System.Windows.Forms.Padding(4);
+            this.bDone.Name = "bDone";
+            this.bDone.Size = new System.Drawing.Size(100, 28);
+            this.bDone.TabIndex = 3;
+            this.bDone.Text = "Готово";
+            this.bDone.UseVisualStyleBackColor = false;
+            this.bDone.Click += new System.EventHandler(this.bDone_Click);
+            // 
+            // cbExitOnStart
+            // 
+            this.cbExitOnStart.AutoSize = true;
+            this.cbExitOnStart.Location = new System.Drawing.Point(16, 107);
+            this.cbExitOnStart.Margin = new System.Windows.Forms.Padding(4);
+            this.cbExitOnStart.Name = "cbExitOnStart";
+            this.cbExitOnStart.Size = new System.Drawing.Size(255, 21);
+            this.cbExitOnStart.TabIndex = 6;
+            this.cbExitOnStart.Text = "Закрыть приложение при запуске";
+            this.cbExitOnStart.UseVisualStyleBackColor = true;
+            this.cbExitOnStart.CheckedChanged += new System.EventHandler(this.cbExitOnStart_CheckedChanged);
+            // 
+            // Settings
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
+            this.ClientSize = new System.Drawing.Size(355, 143);
+            this.Controls.Add(this.cbExitOnStart);
+            this.Controls.Add(this.bDone);
+            this.Controls.Add(this.bPath);
+            this.Controls.Add(this.lPath);
+            this.Controls.Add(this.tbPath);
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Settings";
+            this.Text = "Настройки";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Settings_FormClosing);
+            this.Load += new System.EventHandler(this.Settings_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbPath;
+        private System.Windows.Forms.Label lPath;
+        private System.Windows.Forms.Button bPath;
+        private System.Windows.Forms.Button bDone;
+        private System.Windows.Forms.CheckBox cbExitOnStart;
+    }
+}
---
+++ GRPLauncher/samplauncher/Settings.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+
+namespace SAMPLauncher
+{
+    public partial class Settings : Form
+    {
+
+
+        public Settings()
+        {
+            InitializeComponent();
+            tbPath.Text = ClientInfo.path;
+            cbExitOnStart.Checked = ClientInfo.exitonstart;
+
+            //FIXME: По какой-то причине не работает
+
+            //bDownloadSamp.Enabled = ServerInfo.allowInstallSamp;
+            //if (!ServerInfo.allowInstallSamp) bDownloadSamp.Hide(); 
+
+        }
+
+        private void bPath_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.CheckFileExists = true;
+                ofd.Filter = "SAMP|samp.exe";
+                ofd.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    tbPath.Text = Path.GetDirectoryName(ofd.FileName);
+                    ClientInfo.path = tbPath.Text;
+                }
+            }
+        }
+
+        /*private void bDownloadSamp_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Process.Start(Directory.GetCurrentDirectory() + "/SAMP/install.exe");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }*/
+
+        private void bDone_Click(object sender, EventArgs e)
+        {
+            ClientInfo.path = tbPath.Text;
+            this.Close();
+        }
+
+        private void Settings_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            ClientInfo.path = tbPath.Text;
+        }
+
+        private void cbExitOnStart_CheckedChanged(object sender, EventArgs e)
+        {
+            ClientInfo.exitonstart = cbExitOnStart.Checked;
+        }
+
+        private void Settings_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void tbPath_TextChanged(object sender, EventArgs e)
+        {
+
+            string Game = "samp.exe";
+            bool GameExist = tbPath.Text.Contains(Game);
+
+
+            if (string.IsNullOrEmpty(tbPath.Text))
+            {
+                MessageBox.Show("Не найден файл с игрой!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
